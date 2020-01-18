864ce7d4a510afc57f11c29052cf9693fb9b0f68
onesketchyguy <34846360+onesketchyguy@users.noreply.github.com>
2020-01-18 09:53:49 -0800

    BaseMovemet added

---
+++ Project/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerBehaviour : MonoBehaviour
+    {
+        private new Rigidbody2D rigidbody;
+
+        public string Vertical = "Vertical";
+        public string Horizontal = "Horizontal";
+
+        public float speed = 5;
+
+        private Vector2 input
+        {
+            get
+            {
+                return new Vector2()
+                {
+                    x = Input.GetAxis(Horizontal),
+                    y = Input.GetAxis(Vertical)
+                };
+            }
+        }
+
+        private void Start()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            var n_input = input;
+            n_input.y *= 0.5f;
+
+            rigidbody.MovePosition((Vector2)transform.position + ((n_input * speed) * Time.deltaTime));
+        }
+    }
+}
